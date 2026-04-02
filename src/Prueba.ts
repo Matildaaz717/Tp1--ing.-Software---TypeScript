e56cb43af14ac9a591a364c158611480408892ed
Matildaaz717 <matildaazcona108@gmail.com>
2026-04-02 16:19:20 +0000

    .

---
+++ src/Prueba.ts
@@ -0,0 +1,19 @@
+
+//print 
+console.log ("hola mundo")
+
+//variable 
+var texto = "esto es una cadena de texto"
+console. log(texto)
+
+//crear variables  
+let texto2 = "Esto es una cadena de texto"
+texto2 ="Cambio el valor de la cadena"
+//texto2 = 5 error
+console.log(texto2)
+
+//crear variables con tipo de dato 
+
+let texto2: string ="esto es una cadena de texto"
+
+// export permite que las clases puedan usarse en otros archivos 
