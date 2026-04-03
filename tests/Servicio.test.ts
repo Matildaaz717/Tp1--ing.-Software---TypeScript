7f8f1446e64084dd405689053363894c805e68b7
Matildaaz717 <matildaazcona108@gmail.com>
2026-04-03 08:59:52 +0000

    test servicio

---
+++ tests/Servicio.test.ts
@@ -0,0 +1,33 @@
+import{Servicio} from "../src/Servicio";
+import { describe, expect, test } from "vitest";
+
+describe ("Test de Servicio",() => {
+  
+    test("Crear servicio", () => {
+
+        let servicio= new Servicio ("Esmaltado Semipermanente",0,0);
+
+        expect(servicio.getNombre()). toBe("Esmaltado Semipermanente");
+        expect(servicio.getPrecio()). toBe(7000);
+        expect(servicio.getTiempo()). toBe(1);
+    });
+
+
+    test("No se debe poder cambiar el precio", ()=>{
+        let servicio = new Servicio ("Esmaltado Semipermanente", 100000,10);
+
+        expect(servicio.getPrecio()).toBe(7000);
+        expect(servicio.getTiempo()).toBe(1);
+
+    }); 
+
+    test("Mostrar datos del servicio",()=>{
+
+        let servicio = new Servicio ("Kapping Gel", 0, 0);
+
+        let datos= servicio.mostrarDatos();
+
+        expect(datos).toBe("Servicio:Kapping Gel -Precio:$10000 -Tiempo de Duración:2hrs");
+    });
+
+});
