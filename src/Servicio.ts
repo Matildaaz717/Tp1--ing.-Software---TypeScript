aed2d3d207fe0ead985b37b8815d8cd7e8e38cd1
Matildaaz717 <matildaazcona108@gmail.com>
2026-04-03 08:34:36 +0000

    ;)

--- src/Servicio.ts
+++ src/Servicio.ts
@@ -10,7 +10,30 @@ export class Servicio implements IMostrar{
         this.nombre= nombre;
         this.precio= precio;
         this.tiempo = tiempo; 
+        
+
+        if (precio && tiempo){
+           this.precio= precio;
+           this.tiempo = tiempo;  
+        }
+        else{ 
+            if (nombre==="Esmaltado Semipermanente"){
+                this.precio= 7000;
+                this.tiempo= 1;
+            }
+            else if (nombre==="Kapping Gel"){
+                this.precio= 10000;
+                this.tiempo= 2;
+
+            }
+            else if (nombre=== "Esculpidas"){
+                this.precio= 17000;
+                this.tiempo= 3;
+            }
+        }
     }
+
+    
 
     public getNombre():string {
         return this.nombre;
@@ -23,6 +46,8 @@ export class Servicio implements IMostrar{
     } 
 
     public mostrarDatos(): string {
-        return "Servicio:" + this.nombre + "Precio: $" + this.precio + "Tiempo de Duración: " + this.tiempo + "hrs"
+        return "Servicio:" + this.nombre + 
+               "Precio: $" + this.precio + 
+               "Tiempo de Duración: " + this.tiempo + "hrs"
     }
     }
